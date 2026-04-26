91f0d56ffbdfc97cae8b914efbeee7e30a16754c
Vinicius Alves dos Santos <viniciussantos1605@gmail.com>
2026-04-26 13:25:06 -0300

    fix() - criação da model gerente

--- frontend/src/app/services/gerente-service.ts
+++ frontend/src/app/services/gerente-service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from '../shared/models/cliente.model';
 import { ClienteService } from './cliente-service';
+import { Gerente } from '../shared/models/gerente.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GerenteService {
   constructor(private clienteService: ClienteService) {}
+
+  cpfLogado: String = '';
+
+  // Implementar a lógica para buscar o cpf do gerente logado
+  obtemCpf(gerente: Gerente): string {
+    return gerente.cpf;
+  }
 
   consultar3MelhoresClientes(): Cliente[] {
     const clientes: Cliente[] = [];
