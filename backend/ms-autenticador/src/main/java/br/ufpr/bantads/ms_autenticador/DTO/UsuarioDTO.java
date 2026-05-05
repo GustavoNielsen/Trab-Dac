9c2b21339937a7e0db781cff9614a66d45cba33e
Vinicius Alves dos Santos <viniciussantos1605@gmail.com>
2026-05-04 21:53:37 -0300

    fix() - Corrigino ms-autenticador

---
+++ backend/ms-autenticador/src/main/java/br/ufpr/bantads/ms_autenticador/DTO/UsuarioDTO.java
@@ -0,0 +1,17 @@
+package br.ufpr.bantads.ms_autenticador.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDTO {
+    private Long id;
+    private String cpf;
+    private String nome;
+    private String email;
+}
--- backend/ms-autenticador/src/main/java/br/ufpr/bantads/ms_autenticador/repository/AuthRepository.java
+++ backend/ms-autenticador/src/main/java/br/ufpr/bantads/ms_autenticador/repository/AuthRepository.java
@@ -3,7 +3,8 @@ package br.ufpr.bantads.ms_autenticador.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import br.ufpr.bantads.ms_autenticador.UsuarioAuth;
 import java.util.Optional;
+import java.lang.Long;
 
 public interface AuthRepository extends MongoRepository<UsuarioAuth, Long> {
-    Optional<UsuarioAuth> findByLogin(String login);
+    Optional<UsuarioAuth> findByEmail(String email);
 }
